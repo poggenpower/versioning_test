3810f61ca6be5b35884b924825faa68fbf33f9fe
Thomas Laubrock <github@schmu.net>
2021-09-01 22:04:37 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+from copy import copy, deepcopy
+import json
+from collections import defaultdict
+import logging
+import argparse
+import os
+import re
+import operator
+import time
+import signal
+import sys
+import paho.mqtt.client as mqtt
+import yaml
+import prometheus_client as prometheus
+from yamlreader import yaml_load
+import utils.prometheus_additions
+
+__version__ = '1.2.4'
+SUFFIXES_PER_TYPE = {
+    "gauge": [], 
+    "counter": ['total'],
+    "counter_absolute": ['total'],
+    "summary": ['sum', 'count'],
+    "histogram": ['sum', 'count', 'bucket'],
+    "enum": [],
+}
