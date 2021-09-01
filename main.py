424869811987c77fab5c9e8d158670a4851086d5
poggenpower <git@schmu.net>
2021-09-01 23:30:25 +0200

    Bump version: 1.3.2 → 1.3.3

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import prometheus_client as prometheus
 from yamlreader import yaml_load
 import utils.prometheus_additions
 
-1.3.2
+__version__ = '1.3.3'
 SUFFIXES_PER_TYPE = {
     "gauge": [], 
     "counter": ['total'],
